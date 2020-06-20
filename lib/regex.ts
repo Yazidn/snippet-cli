13e0f5966d484e1c249776b756fd0ffe98a50e14
Yazid <secho771@gmail.com>
2020-06-20 15:06:01 +0100

    updated regex

--- lib/regex.ts
+++ lib/regex.ts
@@ -1,6 +1,6 @@
 const _regex = {
     tag: /(@|#)(\w+)/g,
-    last: /(\d*)(day|week|month|year)/g,
+    last: /(\d*)(?<![A-Za-z])(day|week|month|year)/g,
     day: /(sunday|monday|tuesday|wednesday|thursday|friday|saturday)/gi
 };
 
