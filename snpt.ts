a0dbe563a6c99b6b040ac05bd900e934c044f327
Yazid <secho771@gmail.com>
2020-06-22 14:04:12 +0100

    moved remove functions to remove.tts

--- snpt.ts
+++ snpt.ts
@@ -1,5 +1,6 @@
 import search from "./lib/search.ts";
 import write from "./lib/write.ts";
+import { remove_entry } from "./lib/remove.ts";
 import import_export from "./lib/import_export.ts";
 import extras from "./lib/extras.ts";
 
@@ -13,12 +14,19 @@ init_display();
 
 // Search
 if (flags.s || flags.search)
-  display(await search.search_by_text(flags.s || flags.search), flags.s || flags.search);
+  display(
+    await search.search_by_text(flags.s || flags.search),
+    flags.s || flags.search
+  );
 
 if (flags.t || flags.tag)
-  display(await search.search_by_tag(flags.t || flags.tag), flags.t || flags.tag);
+  display(
+    await search.search_by_tag(flags.t || flags.tag),
+    flags.t || flags.tag
+  );
 
-if (flags.d || flags.date) display(await search.is_same(flags.d || flags.date), flags.d || flags.date);
+if (flags.d || flags.date)
+  display(await search.is_same(flags.d || flags.date), flags.d || flags.date);
 
 if ((flags.f && flags.u) || (flags.from && flags.until))
   display(
@@ -28,26 +36,32 @@ if ((flags.f && flags.u) || (flags.from && flags.until))
     `${flags.f || flags.from} ---> ${flags.u || flags.until}`
   );
 
-if (flags.l || flags.last) display(await search.last(flags.l || flags.last), flags.l || flags.last);
+if (flags.l || flags.last)
+  display(await search.last(flags.l || flags.last), flags.l || flags.last);
 
 // Write
 if (flags.w || flags.write || args.length)
   write.write_entry(flags.w || flags.write, {
     on: flags.on,
     at: flags.at,
-    args
+    args,
   });
 
 if (flags.e || flags.edit) write.edit_entry(flags.e || flags.edit, args);
-if (flags.r || flags.remove) write.remove_entry(flags.r || flags.remove, {
-  today: flags.today,
-  last: flags.l || flags.last,
-  date: flags.d || flags.date,
-  between: [flags.f || flags.from, flags.u || flags.until],
-  all: flags.a || flags.all,
-  recent: flags.recent,
-  tag: flags.t || flags.tag
-}, args);
+if (flags.r || flags.remove)
+  remove_entry(
+    flags.r || flags.remove,
+    {
+      today: flags.today,
+      last: flags.l || flags.last,
+      date: flags.d || flags.date,
+      between: [flags.f || flags.from, flags.u || flags.until],
+      all: flags.a || flags.all,
+      recent: flags.recent,
+      tag: flags.t || flags.tag,
+    },
+    args
+  );
 
 // Export & Import
 if (flags.m || flags.markdown)
